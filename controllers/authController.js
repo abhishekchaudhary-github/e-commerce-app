09661b5994a884f47b1133a3126021b7c32db00c
abhishekchaudhary-github <abhishekch.165@gmail.com>
2023-10-24 23:45:03 +0530

    logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,11 +21,30 @@ const register = async (req, res) => {
 }
 
 const login = async (req, res) => {
-    res.send('...')
+    const {email,password} = req.body
+    if (!email || !password) {
+        throw new CustomError.BadRequestError('Please provide email and password');
+      }
+      const user = await User.findOne({ email });
+    
+      if (!user) {
+        throw new CustomError.UnauthenticatedError('Invalid Credentials');
+      }
+      const isPasswordCorrect = await user.comparePassword(password);
+      if (!isPasswordCorrect) {
+        throw new CustomError.UnauthenticatedError('Invalid Credentials');
+      }
+      const tokenUser= {name:user.name,userId:user._id}
+      attactCookiesToResponse(res,tokenUser)
+      res.status(StatusCodes.CREATED).json({tokenUser,token})
 }
 
 const logout = async (req, res) => {
-    res.send('...')
+    res.cookie('token','randomevalue',{
+        httpOnly:true,
+        expires:new Date(Date.now())
+    })
+    res.status(StatusCodes.OK).json({msg:'logged out!'})
 }
 
 module.exports = {
